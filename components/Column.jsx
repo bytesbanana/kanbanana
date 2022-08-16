c3074c87969752e2325fd45e86dc75623dd1ce8e
Saharat Paynok <saharat.paynok@gmail.com>
2022-08-16 23:03:04 +0700

    Handle show manage card modal

--- components/Column.jsx
+++ components/Column.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 import AddNewCardForm from './AddNewCardForm';
+import ManageCardModal from './ManageCardModal';
 
 const ColumnHeader = ({ data }) => {
   if (!data) return <></>;
@@ -20,40 +21,46 @@ const CardList = React.forwardRef(({ children, ...rest }, ref) => (
 CardList.displayName = 'CardList';
 
 const Column = ({ data, boardName }) => {
-  return (
-    <div className='flex  flex-col min-w-[300px] max-w-[300px] p-4' key={data.name}>
-      <ColumnHeader data={data} />
-      <Droppable droppableId={data.id}>
-        {(provided) => (
-          <CardList {...provided.droppableProps} ref={provided.innerRef} className='flex flex-col flex-1 gap-2'>
-            {data.cards.map((card, index) => (
-              <Draggable key={card.id} draggableId={card.id} index={index} id={card.id}>
-                {(provided) => (
-                  <li
-                    key={card.id}
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className='p-4 rounded-md select-none bg-neutral-800 hover:bg-neutral-700'
-                  >
-                    <h2 className='text-lg font-semibold tracking-wide text-slate-300'>{card.title}</h2>
-                    {card.total === 0 && <p className='text-sm text-slate-500'>have no subtask</p>}
-                    {card.total > 0 && (
-                      <p className='text-sm text-slate-500'>
-                        {card.done} of {card.total} subtasks
-                      </p>
-                    )}
-                  </li>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
+  const [showManageCard, setShowManageCard] = useState(false);
 
-            <AddNewCardForm boardName={boardName} columnId={data.id} />
-          </CardList>
-        )}
-      </Droppable>
-    </div>
+  return (
+    <>
+      <div className='flex  flex-col min-w-[300px] max-w-[300px] p-4' key={data.name}>
+        <ColumnHeader data={data} />
+        <Droppable droppableId={data.id}>
+          {(provided) => (
+            <CardList {...provided.droppableProps} ref={provided.innerRef} className='flex flex-col flex-1 gap-2'>
+              {data.cards.map((card, index) => (
+                <Draggable key={card.id} draggableId={card.id} index={index} id={card.id}>
+                  {(provided) => (
+                    <li
+                      key={card.id}
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      className='p-4 rounded-md select-none bg-neutral-800 hover:bg-neutral-700'
+                      onClick={() => setShowManageCard(true)}
+                    >
+                      <h2 className='text-lg font-semibold tracking-wide text-slate-300'>{card.title}</h2>
+                      {card.total === 0 && <p className='text-sm text-slate-500'>have no subtask</p>}
+                      {card.total > 0 && (
+                        <p className='text-sm text-slate-500'>
+                          {card.done} of {card.total} subtasks
+                        </p>
+                      )}
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+
+              <AddNewCardForm boardName={boardName} columnId={data.id} />
+            </CardList>
+          )}
+        </Droppable>
+        {!showManageCard && <ManageCardModal />}
+      </div>
+    </>
   );
 };
 
